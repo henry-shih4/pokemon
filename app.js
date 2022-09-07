91c2d9139ba4257baa6cd0ffc3b678eacf723562
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-09-06 23:54:35 -0500

    chart
    added google chart

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ let sprite = document.getElementById("image");
 let nameDisplay = document.getElementById("name");
 let numDisplay = document.getElementById("num");
 let typesDisplay = document.getElementById("type");
-let chartDisplay = document.getElementById('chart-container');
+let chartDisplay = document.getElementById("chart-container");
 let randomBtn = document.getElementById("random-btn");
 let pokemonContainer = document.getElementById("pokemon");
 let pokemonSearch = document.getElementById("search-name");
 let form = document.getElementById("form");
 let regExLetters = /[A-Za-z0-9]/gi;
+let individualStat = [["stat", ""]];
 
 form.addEventListener("submit", searchPokemon);
 randomBtn.addEventListener("click", loading);
@@ -27,7 +28,6 @@ function fetchPokemon() {
 }
 
 function searchPokemon() {
-  chartDisplay.innerHTML='';
   event.preventDefault();
   id = pokemonSearch.value.toLowerCase();
   if (id == "") {
@@ -42,56 +42,37 @@ function searchPokemon() {
       })
       .then((json) => displayPokemon(json))
       .catch((err) => errorMessage());
-    }
-   else {
-    sprite.innerHTML = "<div>Please enter a pokemon name or Pokedex number.</div>";
+  } else {
+    sprite.innerHTML =
+      "<div>Please enter a pokemon name or Pokedex number.</div>";
   }
 }
 
 function displayPokemon(pokemon) {
+  individualStat = [["stat", ""]];
   id = pokemon.id;
   sprite.innerHTML = `<div id="question-mark"><img class='moving' src="images/question-mark.png" alt=""></div>`;
   let name = pokemon.name;
   let types = pokemon.types;
   let pokemonStats = pokemon.stats;
-  let individualStat = [];
   let pokemonTypes = [];
-
 
   types.forEach((type) => {
     pokemonTypes.push(type.type.name);
   });
 
-  console.log(pokemonStats);
+  pokemonStats.forEach((statType) => {
+    individualStat.push([statType.stat.name, statType["base_stat"]]);
+  });
 
-  pokemonStats.forEach((statType)=>{
-    individualStat.push([statType.stat.name, statType['base_stat']]);
-  }
-  )
- 
-  anychart.onDocumentReady(function(){
-    const pokemonStatData = {
-      header: ["Stat", "Amount"],
-      rows: individualStat
-    }
-
-
-  var chart = anychart.bar();
-  chart.data(pokemonStatData);
-  chart.title('pokemon stats');
-  chart.container('chart-container');
-  chart.normal().fill('red',0.5);
-  chart.draw();
-});
-
-
-  console.log(individualStat)
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawChart);
 
   sprite.innerHTML = `<img id="poke-sprite" src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png>`;
   nameDisplay.innerHTML = `<div id='name-display'>${
     name.charAt(0).toUpperCase() + name.slice(1)
   }</div>`;
-  numDisplay.innerHTML = `<div id="num-display">#${id}</div>`;
+  numDisplay.innerHTML = `<div id="num-display">Pokedex Entry #${id}</div>`;
   typesDisplay.innerHTML = `<div id="type-display">Type(s): ${pokemonTypes.join(
     ",  "
   )}</div>`;
@@ -110,7 +91,6 @@ function loading() {
   nameDisplay.innerHTML = "";
   typesDisplay.innerHTML = "";
   numDisplay.innerHTML = "";
-  chartDisplay.innerHTML = '';
 
   let movingImage = document.querySelectorAll(".moving");
 
@@ -124,4 +104,32 @@ function loading() {
 function errorMessage() {
   sprite.innerHTML = "<div>Pokemon not found. Try again.</div>";
   console.error("Fetch problem: pokemon not found");
+}
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(individualStat);
+
+  var options = {
+    title: "Pokemon Stats",
+    chartArea: { top: 20, bottom: 0, right: 0, left: 50 },
+    // legend: { position: "none" },
+    hAxis: { gridlines: { count: 2 } },
+    // vAxis: { textPosition: "none", gridlines: { count: 0 } },
+    baselineColor: "transparent",
+    bar: { groupWidth: "95%" },
+    colors: ["#657CD0", "#DA68A0", "#06C3C0", "#777B80", "#7C6D70", "#7C0850"],
+  };
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([
+    0,
+    1,
+    { calc: "stringify", sourceColumn: 1, type: "string", role: "annotation" },
+  ]);
+
+  var chart = new google.visualization.BarChart(
+    document.getElementById("myChart")
+  );
+
+  chart.draw(view, options);
 }
